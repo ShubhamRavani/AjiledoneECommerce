509d23c15e93a7b0d8805bb8ad0b7feb25b85cd0
Shubham Ravani <shubhamravani67890@gmail.com>
2022-11-21 22:06:35 +0530

    Ajiledone ECommerce

--- src/components/Product1.jsx
+++ src/components/Product1.jsx
@@ -1,25 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './Product1.css';
 import axios from "axios";
 
 const Product1 = () => {
     const [products, setProducts] = useState([]);
 
-    const options = {
-        method: 'GET',
-        url: process.env.REACT_APP_AMAZON_API_URL,
-        params: { categoryID: 'aps', keyword: 'iphone', country: 'US', page: '1' },
-        headers: {
-            'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-            'X-RapidAPI-Host': process.env.REACT_APP_AMAZON_RAPIDAPI_HOST
-        }
-    };
-    axios.request(options).then(function (response) {
-        setProducts(response.data.docs);
-    }).catch(function (error) {
-        console.error(error);
-    });
-
+    useEffect(() => {
+        axios.get('https://dummyjson.com/products/')
+            .then(response => {
+                setProducts(response.data.products)
+                console.warn(response.data.products)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, []);
 
     return(
         <>
@@ -28,12 +23,12 @@ const Product1 = () => {
         </div>
 
         <div className="main-card">
-        {products.map((docs, index) => {
+        {products.map((data, index) => {
             return (
                 <div key={index} className='card' type="none">
-                    <img className="posterimage" src={docs.product_main_image_url} alt="imageposter" ></img>
-                    <p className="line2">{docs.product_title} </p>
-                    <p className="line3"><b>{docs.app_sale_price}</b></p>
+                    <img className="posterimage" src={data.images} alt="imageposter" ></img>
+                    <p className="line2">{data.title} </p>
+                    <p className="line3"><b>{data.price}</b></p>
 
                 </div>
 
